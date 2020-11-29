4f28f55a73071bce1d58282ca755e94bc7c1590c
Morten Ørris Poulsen <morten.oerris.poulsen@LEGO.com>
2020-11-29 23:14:35 +0100

    extend_range() done

--- apm_5.jl
+++ apm_5.jl
@@ -1,24 +1,20 @@
-# --------------------------------------------------------------------------
-# Julia Implementation of the R code in
-# Applied Predictive Modeling
-# Chapter 5, Measuring Performance in Regression Models
-#
-# by Morten Ørris Poulsen
-# --------------------------------------------------------------------------
+#= -------------------------------------------------------------------------
+    Julia Implementation of the R code in Applied Predictive Modeling
+    Chapter 5, Measuring Performance in Regression Models
+    by Morten Ørris Poulsen
+
+    This is an adaption – or update – of the code in section 5.3 of Applied
+    Predictive Modeling. The aim is to use Julia for the code.
+------------------------------------------------------------------------- =#
 
 
-# Introduction -------------------------------------------------------------
-# This is an adaption – or update – of the code in section 5.3 of Applied
-# Predictive Modeling. The aim is to use Julia for the code.
-
-
-# Lessons Learned ----------------------------------------------------------
-# - The min() and max() functions should be replaced by minimum() and maximum()
-#   as the former do not work as expected on arrays.
-
-# - The is an r²() function i StatsBase, but it takes an obj::StatisticalModel
-#   as input. Have not yet succeeded in creating this, so for now it is
-#   calculated using the square of the correlation.
+#= Lessons Learned ---------------------------------------------------------
+    - The min() and max() functions should be replaced by minimum() and
+    maximum() as the former do not work as expected on arrays.
+    - The is an r²() function i StatsBase, but it takes an
+    obj::StatisticalModel as input. Have not yet succeeded in creating this,
+    so for now it is calculated using the square of the correlation.
+------------------------------------------------------------------------- =#
 
 
 # Setup --------------------------------------------------------------------
@@ -52,14 +48,43 @@ all_numbers = vcat(observed, predicted)
 maximum(all_numbers)
 minimum(all_numbers)
 
-summarystats(residuals)
-describe(residuals)
+StatsBase.summarystats(residuals)
+StatsBase.describe(residuals)
 
-# TODO Consider extending the range artificially similar to extendedrange() in R
+#= Consider extending the range artificially similar to extendedrange() in R
+https://www.rdocumentation.org/packages/grDevices/versions/3.6.2/topics/extendrange
+=#
+
+# This version is a copy of the R function but not done
+function extend_range(x::Array, r = maximum(x) - minimum(x), f::Float64 = 0.05)::Array
+    extension = r * f
+    start = r[1] - extension
+    finish = r[2] + extension
+    return [start, finish]
+end
+
+# This is a simpler version and works
+function extend_range(x::Array, f::Float64 = 0.05)::Array
+    highest = maximum(x)
+    lowest = minimum(x)    
+    range = highest - lowest
+    extension = range * f
+    start = lowest - extension
+    finish = highest + extension
+    return [start, finish]
+end
+    
+new_range = extend_range(all_numbers)
+extend_range([1, 7, 5])
 
 # Plot using Gadfly with a data frame
 data = DataFrames.DataFrame(obs = observed, pred = predicted)
 Gadfly.plot(data, x = :obs, y = :pred, Geom.point)
+
+# The same but with a prettier range
+Gadfly.plot(data, x = :obs, y = :pred, 
+            Coord.cartesian(xmin=new_range[1], xmax=new_range[2], ymin=new_range[1], ymax=new_range[2]),
+            Geom.point)
 
 # The same using the arrays directly
 Gadfly.plot(x = observed, y = predicted, color = [HSL(0, 0, 0.7)], Geom.point,
