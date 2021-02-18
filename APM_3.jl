1da3b4aad710737ee96b79caad5fabb03646d93d
Morten Ørris Poulsen <morten.oerris.poulsen@LEGO.com>
2021-02-18 20:29:47 +0100

    na

--- APM_3.jl
+++ APM_3.jl
@@ -1,15 +1,17 @@
-# --------------------------------------------------------------------------
-# Julia implementation of the R code in Applied Predictive Modeling
-# Chapter 3 – Data Pre-processing
-# Code from chapter 3.8
-# --------------------------------------------------------------------------
+"""
+Julia implementation of the R code in Applied Predictive Modeling
+Chapter 3 – Data Pre-processing
+Code from chapter 3.8
 
+# Notes
+
+"""
 
 # Setup --------------------------------------------------------------------
+using BoxCoxTrans
 using DataFrames
 using DataFramesMeta
 using RData
-using Statistics
 using StatsBase
 
 
@@ -24,26 +26,31 @@ cell = segdata[:Cell]
 class = segdata[:Class]
 case = segdata[:Case]
 
-# TODO Remove columns
+# Remove columns 1 to 3
 segdata = segdata[:, 4:end]
 
-# With Julia 1.0, contains() is replaced by occursin() for strings
+# Remove the status columns
 # Note the dot to operate on the items individually
 status_columns = occursin.("Status", string.(names(segdata)))
 
+# countmap(status_columns)
+select!(segdata, Not(names(segdata)[status_columns]))
 
-status_columns .== true
 
-countmap(status_columns)
+#= Transform data =#
 
-# Transform data -----------------------------------------------------------
+# Calculate skewness
 StatsBase.skewness(segdata[:AngleCh1])
 
 aggregate(segdata, skewness)
 
-aggregate(segdata, :Cell, skewness)
+Ch1AreaTrans = BoxCoxTrans.transform(segdata.AreaCh1)
+BoxCoxTrans.lambda(segdata.AreaCh1).value
 
-head(segdata[[:AreaCh1]])
+
+#aggregate(segdata, :Cell, skewness)
+
+#head(segdata[[:AreaCh1]])
 
 
 # Filter data --------------------------------------------------------------
@@ -59,3 +66,67 @@ head(segdata[[:AreaCh1]])
 
 
 # Exercise 3.3 -------------------------------------------------------------
+
+
+
+using Plots
+plotly() # using plotly for 3D-interacive graphing
+
+
+# split half to training set
+Xtr = convert(Array, iris[1:2:end, 1:4])
+Xtr_labels = convert(Array, iris[1:2:end, 5])
+
+# split other half to testing set
+Xte = convert(Array, iris[2:2:end, 1:4])
+Xte_labels = convert(Array, iris[2:2:end, 5])
+
+
+
+# apply PCA model to testing set
+Yte = MultivariateStats.transform(M, Xte)
+
+# reconstruct testing observations (approximately)
+Xr = reconstruct(M, Yte)
+
+# group results by testing set labels for color coding
+setosa = Yte[:, Xte_labels.=="setosa"]
+versicolor = Yte[:, Xte_labels.=="versicolor"]
+virginica = Yte[:, Xte_labels.=="virginica"]
+
+# visualize first 3 principal components in 3D interacive plot
+p = scatter(setosa[1,:],setosa[2,:],setosa[3,:],marker=:circle,linewidth=0)
+scatter!(versicolor[1,:],versicolor[2,:],versicolor[3,:],marker=:circle,linewidth=0)
+scatter!(virginica[1,:],virginica[2,:],virginica[3,:],marker=:circle,linewidth=0)
+plot!(p, xlabel="PC1", ylabel="PC2", zlabel="PC3")
+
+
+
+#=
+PCA using MLJ.jl
+=#
+using MLJ
+using MLJMultivariateStatsInterface
+
+X, y = @load_iris
+#schema(X)
+
+train, test = partition(eachindex(y), 0.50, shuffle=true, rng = 7390)
+# ([125, 100, 130, 9, 70, 148, 39, 64, 6, 107  …  110, 59, 139, 21, 112, 144, 140, 72, 109, 41], [106, 147, 47, 5])
+# Instantiate and fit the model/machine:
+
+# prøve = @load PCA
+pca = PCA(maxoutdim = 3)
+mach = machine(pca, X)
+fit!(mach, rows = train)
+
+#Transform selected data bound to the machine:
+y_test = transform(mach, rows=test)
+
+#Transform new data:
+Xnew = (sepal_length=rand(3), sepal_width=rand(3),
+        petal_length=rand(3), petal_width=rand(3))
+
+transform(mach, Xnew)
+#(x1 = [4.316489556017327, 4.238914682299775, 5.342670097730145],
+# x2 = [-5.075190145248834, -5.225553878737216, -5.318389797856577],)
